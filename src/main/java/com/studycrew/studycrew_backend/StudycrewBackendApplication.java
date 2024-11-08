d03c59d32545cdf24c39c0f934a1d20ca65ee323
leeminjung1 <vipminjung@gmail.com>
2024-11-08 17:36:30 +0900

    feat: initial setup for backend project - set up application.properties with database and server configs - configured JPA and logging - added basic CORS setup

---
+++ src/main/java/com/studycrew/studycrew_backend/StudycrewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studycrew.studycrew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudycrewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudycrewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studycrew/studycrew_backend/StudycrewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studycrew.studycrew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudycrewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
