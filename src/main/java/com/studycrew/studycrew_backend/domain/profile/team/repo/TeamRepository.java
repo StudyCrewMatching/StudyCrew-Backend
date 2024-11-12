6887ff69a0476d02202b6a1ac53fee79b80707af
imzero238 <nayoung8142@gmail.com>
2024-11-12 12:26:21 +0900

    Refactor: 팀 삭제 JPA에 @Query 설정

--- src/main/java/com/studycrew/studycrew_backend/domain/profile/team/repo/TeamRepository.java
+++ src/main/java/com/studycrew/studycrew_backend/domain/profile/team/repo/TeamRepository.java
@@ -2,10 +2,13 @@ package com.studycrew.studycrew_backend.domain.profile.team.repo;
 
 import com.studycrew.studycrew_backend.domain.profile.team.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
-    Optional<Team> findByIdAndLeaderId(Long id, Long leaderId);
+    @Query("select t from Team t where t.id=:id and t.leader.id=:leaderId")
+    Optional<Team> findByIdAndLeaderId(@Param("id") Long id, @Param("leaderId") Long leaderId);
 }
